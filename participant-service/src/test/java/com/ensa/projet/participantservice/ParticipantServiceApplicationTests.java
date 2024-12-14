e356480b5faaf458be78c4dccc9291570b183439
chouay walid <chouaywalid15@gmail.com>
2024-12-14 18:22:42 +0100

    commit test

--- participant-service/src/test/java/com/ensa/projet/participantservice/ParticipantServiceApplicationTests.java
+++ participant-service/src/test/java/com/ensa/projet/participantservice/ParticipantServiceApplicationTests.java
@@ -2,6 +2,9 @@ package com.ensa.projet.participantservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class ParticipantServiceApplicationTests {
--- training-service/src/test/java/com/ensa/projet/trainingservice/TrainingServiceApplicationTests.java
+++ training-service/src/test/java/com/ensa/projet/trainingservice/TrainingServiceApplicationTests.java
@@ -2,6 +2,9 @@ package com.ensa.projet.trainingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class TrainingServiceApplicationTests {
